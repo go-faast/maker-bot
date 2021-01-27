5d9ac970ff3daa42acd981512f51a894727490f1
dylanseago <dylan@bitaccess.co>
2021-01-26 19:00:07 -0500

    Add docker sign to release script

--- bin/docker_release.sh
+++ bin/docker_release.sh
@@ -5,4 +5,7 @@ VERSION=$(node -e "console.log(require('./package.json').version)")
 docker tag maker-bot:$VERSION faasthub/maker-bot:$VERSION
 docker tag maker-bot:$VERSION faasthub/maker-bot:latest
 
+docker trust sign --local faasthub/maker-bot:$VERSION
+docker trust sign --local faasthub/maker-bot:latest
+
 docker push -a faasthub/maker-bot
