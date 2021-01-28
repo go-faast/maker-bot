f89186e13f0541f00cd393f8eeb098eaf211ee4f
dylanseago <dylan@bitaccess.co>
2021-01-28 00:02:33 -0500

    Remove redundant docker push

--- bin/docker_release.sh
+++ bin/docker_release.sh
@@ -7,5 +7,3 @@ docker tag maker-bot:$VERSION faasthub/maker-bot:latest
 
 docker trust sign --local faasthub/maker-bot:$VERSION
 docker trust sign --local faasthub/maker-bot:latest
-
-docker push -a faasthub/maker-bot
